4242db7dabe7ddc9bfb63a5b87222a07b04422c4
Yannick <y.morizot@numericable.com>
2022-02-12 16:02:25 +0100

    models and admin part

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,18 +1,25 @@
 
+from re import A
 from django.shortcuts import get_object_or_404, render
 from .models import Book
 from django.http import Http404
+from django.db.models import Avg
 
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+
     return render(request, "book_outlet\index.html", {
-    "books" : books
+    "books" : books,
+    "total_number_of_books" : num_books,
+    "average_rating" : avg_rating,
     })
 
-def book_detail(request, id):
-    book = get_object_or_404(Book, pk=id)
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html",{
     "title": book.title,
     "author": book.author,
